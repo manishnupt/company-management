db739e385292b2b6506091f6b028d590f0e85874
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-04-10 17:03:12 +0530

    fix

--- src/main/java/com/hrms/company_management/repository/RoleGroupRepo.java
+++ src/main/java/com/hrms/company_management/repository/RoleGroupRepo.java
@@ -3,9 +3,11 @@ package com.hrms.company_management.repository;
 import com.hrms.company_management.entity.Role;
 import com.hrms.company_management.entity.RoleGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleGroupRepo extends JpaRepository<RoleGroup ,Long> {
 
     Optional<RoleGroup> findByName(String name);
